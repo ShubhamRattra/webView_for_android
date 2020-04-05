ec5654b21aa617e2e6f9050553945774f882870b
Shubham <shubhamrattra@gmail.com>
2020-04-05 09:16:35 +0530

    first commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.webview;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings= mywebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebView.loadUrl("https://www.google.com/");
+        // Line of Code for opening links in app
+        mywebView.setWebViewClient(new WebViewClient());
+    }
+
+    //Code For Back Button
+    @Override
+    public void onBackPressed() {
+        if(mywebView.canGoBack())
+        {
+            mywebView.goBack();
+        }
+
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+}
